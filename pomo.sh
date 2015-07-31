90329aeef763405c321eef4fd0d4e442eb8f4391
Lloyd Moore <manchesterboy@gmail.com>
2015-07-31 15:13:17 +0100

    updated usage information

--- pomo.sh
+++ pomo.sh
@@ -4,7 +4,9 @@
 
 # Usage: pomo
 function usage {
-    echo "usage: $program [minutes]"
+    echo "usage: $0 [-h] [-m minutes]"
+    echo "-h shows this message"
+    echo "-m sets number of minutes to wait"
 }
 
 OPTIND=1
@@ -12,7 +14,7 @@ OPTIND=1
 MINS=$1
 : ${MINS:=25}
 
-while getopts "hm" opt; do
+while getopts "hm:" opt; do
     case "$opt" in
     h)
         usage
@@ -26,4 +28,4 @@ done
 
 shift $((OPTIND-1))
 
-sleep $((60 * $MINS));say "Time up, time up"
+sleep $((60 * $MINS)); say "Time up, time up"
